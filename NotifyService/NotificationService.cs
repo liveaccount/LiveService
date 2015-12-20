8058e6d4a31ffc44bfe3ac204e360d0719289719
liveaccount <liveaccount@live.ru>
2015-12-20 02:46:48 +0200

    +102

--- NotifyService/NotificationService.cs
+++ NotifyService/NotificationService.cs
@@ -1,11 +1,23 @@
 ﻿namespace NotifyService
 {
     using System;
+    using System.Net;
+    using System.Net.Sockets;
     
     public class NotificationService : INotificationService
     {
+        private string guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+        
         public String GetNotification(String name)
         {
+            var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+            serverSocket.Listen(128);
+            //serverSocket.BeginAccept(null, 0, OnAccept, null);
+
+
+
+
             return String.Format("Notification for {0}", name);
         }
     }
