854fb8b42a84c022a4fef4093e74af34f18d0529
liveaccount <liveaccount@live.ru>
2015-12-20 02:35:17 +0200

    +100

---
+++ NotifyServiceClient/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NotifyServiceClient
+{
+    using System;
+
+    using NotifyServiceReference;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var service = new NotificationServiceClient())
+            {
+                Console.WriteLine("Enter your name: ");
+                Console.WriteLine(service.GetNotification(Console.ReadLine()));
+            }
+
+            Console.ReadKey();            
+        }
+    }
+}
