b34f1bde19c180d78a9cbec0c0f42a9974826c98
liveaccount <liveaccount@live.ru>
2015-12-20 03:10:42 +0200

    +103

--- NotifyService/NotificationService.cs
+++ NotifyService/NotificationService.cs
@@ -1,21 +1,30 @@
 ﻿namespace NotifyService
 {
+    using SuperSocket.SocketBase;
+    using SuperSocket.SocketBase.Config;
+    using SuperSocket.SocketEngine;
+    using SuperWebSocket;
     using System;
     using System.Net;
     using System.Net.Sockets;
+    using System.Threading;
     
     public class NotificationService : INotificationService
     {
-        private string guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-        
+
         public String GetNotification(String name)
         {
-            var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
-            serverSocket.Listen(128);
-            //serverSocket.BeginAccept(null, 0, OnAccept, null);
+            var wsServer = new WebSocketServer();
 
+            wsServer.Setup(new RootConfig(), new ServerConfig
+            {
+                Name = "SuperWebSocket",
+                Ip = "Any",
+                Port = 8181,
+                Mode = SocketMode.Tcp,
+            }, new SocketServerFactory());
 
+            wsServer.Start();
 
 
             return String.Format("Notification for {0}", name);
