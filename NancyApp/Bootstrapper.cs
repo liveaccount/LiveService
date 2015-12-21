53f1380a6ec55f6198caa954cb0f64f95320ab92
liveaccount <liveaccount@live.ru>
2015-12-21 18:33:05 +0200

    +147

--- NancyApp/Bootstrapper.cs
+++ NancyApp/Bootstrapper.cs
@@ -1,36 +1,17 @@
 ﻿namespace NancyApp
 {
     using Nancy;
+    using Nancy.TinyIoc;
+    using Nancy.Bootstrapper;
+
     using WebSocketSharp;
     using WebSocketSharp.Server;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
-        private readonly WebSocketServer ws;
-
-        public Bootstrapper()
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
-            ws = new WebSocketServer(System.Net.IPAddress.Parse("50.17.211.206"), 80);
-            ws.AddWebSocketService<Laputa>("/Laputa");
-        }
-
-        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
-        {
-            ws.Start();
-
             base.ApplicationStartup(container, pipelines);
-        }
-    }
-
-    public class Laputa : WebSocketBehavior
-    {
-        protected override void OnMessage(MessageEventArgs e)
-        {
-            var msg = e.Data == "BALUS"
-                      ? "I've been balused already..."
-                      : "I'm not available now.";
-
-            Send(msg);
         }
     }
 }
--- NancyApp/IndexModule.cs
+++ NancyApp/IndexModule.cs
@@ -1,20 +1,60 @@
 ﻿namespace NancyApp
 {
+    using System;
+    
     using Nancy;
+
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
 
     public class IndexModule : NancyModule
     {
+        private readonly HttpServer server;
+
         public IndexModule()
         {
+            server = new HttpServer();
+            server.AddWebSocketService<Test>("/websocket");
+
             Get["/"] = parameters =>
             {
                 return View["index"];
             };
 
-            Get["/Laputa"] = parameters =>
+            Get["/websocket"] = parameters =>
             {
-                return 200;
+                try
+                {
+                    server.Start();
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+
+                var message = String.Empty;
+
+                if (server.IsListening)
+                {
+                    message = String.Format("Server started on port {0}, and providing services:{1}", server.Port, "<br>");
+                    foreach (var path in server.WebSocketServices.Paths)
+                        message += String.Format("- {0}{1}", path, "<br>");
+                }
+
+                return message;
             };
+        }
+    }
+
+    public class Test : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var msg = e.Data == "Test"
+                      ? "Test OK."
+                      : "Test FAILED...";
+
+            Send(msg);
         }
     }
 }
