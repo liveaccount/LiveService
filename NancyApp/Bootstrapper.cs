412aafa21c6ba11e589d77c6fd17d981ffb7be6d
liveaccount <liveaccount@live.ru>
2015-12-21 15:20:07 +0200

    +142

---
+++ NancyApp/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿namespace NancyApp
+{
+    using Nancy;
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        private readonly WebSocketServer ws;
+
+        public Bootstrapper()
+        {
+            ws = new WebSocketServer();
+            ws.AddWebSocketService<Laputa>("/Laputa");
+        }
+
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            ws.Start();
+
+            base.ApplicationStartup(container, pipelines);
+        }
+    }
+
+    public class Laputa : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var msg = e.Data == "BALUS"
+                      ? "I've been balused already..."
+                      : "I'm not available now.";
+
+            Send(msg);
+        }
+    }
+}
